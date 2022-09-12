132c9c135070613a8a4953bd13b1aecb5e6b8041
FerostMask <519274834@qq.com>
2022-09-12 17:55:44 +0800

    日期：2022-09-12 作者：吉平.「集」 版本：V0.0.2.4 修改内容：
        -- 1. 删除主函数中GPIO功能测试代码。
    
    ********************************************************

--- main/main.c
+++ main/main.c
@@ -2,18 +2,10 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#include "drv_gpio.h"
-
 void app_main(void)
 {
-	stc_gpio_config_t io16 = {
-		.ioNum = IO_0,
-		.mode = IO_MODE_INPUT,
-		.pullMode = IO_PULLUP_ONLY,
-	};
-	drv_gpio_init(&io16);
     while (true) {
-        printf("io16 level: %d\n", drv_gpio_get_level(io16.ioNum));
+        printf("Hello from app_main!\n");
         sleep(1);
     }
 }
