75184f3ebe32c87bd16c01bcd73b9f346ea00543
FerostMask <519274834@qq.com>
2022-09-12 13:10:05 +0800

    日期：2022-09-12 作者：吉平.「集」 版本：V0.0.2.3 修改内容：
        -- 1. 完善部分GPIO类型定义。
        -- 2. 创建GPIO初始化接口，实现部分功能。
        -- 3. 添加GPIO获取管脚电平状态、设置管脚电平状态接口。
    
    ********************************************************

--- easyDrv/drv_gpio.h
+++ easyDrv/drv_gpio.h
@@ -11,11 +11,22 @@
 //?------------------------------------------------------------------------------
 //? Includes
 
+#include "drv_gpio_type.h"
+#include "hal/gpio_ll.h"
+
 //?------------------------------------------------------------------------------
 //? Definitions
 
+typedef struct
+{
+    en_gpio_num_t ioNum;
+    en_gpio_mode_t mode;
+} stc_gpio_config_t;
 //?------------------------------------------------------------------------------
 //? Function declarations
+void drv_gpio_set_level(en_gpio_num_t ioNum, uint32_t level);
+int drv_gpio_get_level(en_gpio_num_t ioNum);
+void drv_gpio_init(stc_gpio_config_t * const config);
 
 #endif
 
--- main/main.c
+++ main/main.c
@@ -2,10 +2,18 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+#include "drv_gpio.h"
+
 void app_main(void)
 {
+	stc_gpio_config_t io16 = {
+		.ioNum = IO_0,
+		.mode = IO_MODE_INPUT_OUTPUT,
+	};
+	drv_gpio_init(&io16);
+	drv_gpio_set_level(io16.ioNum, 1);
     while (true) {
-        printf("Hello from app_main!\n");
+        printf("io16 level: %d\n", drv_gpio_get_level(io16.ioNum));
         sleep(1);
     }
 }
